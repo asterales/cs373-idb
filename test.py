46af233abf14357c194695e1fdbff3838b4965ef
StormyEmery <stormyemery12@utexas.edu>
2016-03-24 07:46:21 -0500

    updated unit test to reflect changes in models.py, closes #5

--- test.py
+++ test.py
@@ -38,10 +38,12 @@ class ModelsTest(TestCase) :
     		id=1,
     		name='United States of America',
     		capital='Austin',
-    		population=321645000,
     		lat=38,
     		lng=-97,
-    		region_id=1
+    		region_id=1,
+    		subregion_id=4,
+    		area=9629091,
+    		population=321645000
     	)
 
     	db.session.add(country)
@@ -52,10 +54,12 @@ class ModelsTest(TestCase) :
     	assert c.id == 1
     	assert c.name == 'United States of America'
     	assert c.capital == 'Austin'
-    	assert c.population == 321645000
     	assert c.lat == 38
     	assert c.lng == -97
     	assert c.region_id == 1
+    	assert c.subregion_id == 4
+    	assert c.area == 9629091 
+    	assert c.population == 321645000
 
     def test_countries_2(self) :
 
@@ -63,10 +67,12 @@ class ModelsTest(TestCase) :
     		id=1,
     		name='Afghanistan',
     		capital='Kabul',
-    		population=26023100,
     		lat=33,
     		lng=65,
-    		region_id=2
+    		region_id=2,
+    		subregion_id=3,
+    		area=652230,
+    		population=26023100
     	)
 
     	db.session.add(country)
@@ -77,10 +83,12 @@ class ModelsTest(TestCase) :
     	assert c.id == 1
     	assert c.name == 'Afghanistan'
     	assert c.capital == 'Kabul'
-    	assert c.population == 26023100
     	assert c.lat == 33
     	assert c.lng == 65
     	assert c.region_id == 2
+    	assert c.subregion_id == 3
+    	assert c.area == 652230
+    	assert c.population == 26023100
 
     def test_countries_3(self):
 
@@ -88,10 +96,12 @@ class ModelsTest(TestCase) :
     		id=1,
     		name='Albania',
     		capital='Tirana',
-    		population=2893005,
     		lat=41,
     		lng=20,
-    		region_id=3
+    		region_id=3,
+    		subregion_id=6,
+    		area=28748,
+    		population=2893005
     	)
 
     	db.session.add(country)
@@ -102,10 +112,12 @@ class ModelsTest(TestCase) :
     	assert c.id == 1
     	assert c.name == 'Albania'
     	assert c.capital == 'Tirana'
-    	assert c.population == 2893005
     	assert c.lat == 41
     	assert c.lng == 20
     	assert c.region_id == 3
+    	assert c.subregion_id == 6
+    	assert c.area == 28748
+    	assert c.population == 2893005
 
 
     # ---------------
@@ -125,12 +137,14 @@ class ModelsTest(TestCase) :
 
     	assert l.id == 1
     	assert l.name == None
+    	assert l.iso_code == None
 
     def test_languages_1(self):
 
     	language = Languages(
     		id=1,
-    		name='en'
+    		name='English',
+    		iso_code='en'
     	)	
 
     	db.session.add(language)
@@ -139,13 +153,15 @@ class ModelsTest(TestCase) :
     	l = db.session.query(Languages).filter_by(id=1).first()
 
     	assert l.id == 1
-    	assert l.name == 'en'
+    	assert l.name == 'English'
+    	assert l.iso_code == 'en'
 
     def test_languages_2(self):
 
     	language = Languages(
     		id=1,
-    		name='ps'
+    		name='Chinese',
+    		iso_code='zh'
     	)	
 
     	db.session.add(language)
@@ -154,7 +170,8 @@ class ModelsTest(TestCase) :
     	l = db.session.query(Languages).filter_by(id=1).first()
 
     	assert l.id == 1
-    	assert l.name == 'ps'
+    	assert l.name == 'Chinese'
+    	assert l.iso_code == 'zh'
 
 
     # ---------------
@@ -174,12 +191,14 @@ class ModelsTest(TestCase) :
 
     	assert c.id == 1
     	assert c.name == None
+    	assert c.code == None
 
     def test_currencies_1(self):
 
     	currency = Currencies(
     		id=1,
-    		name='USD'
+    		name='US Dollar',
+    		code='USD'
     	)
 
     	db.session.add(currency)
@@ -188,13 +207,15 @@ class ModelsTest(TestCase) :
     	c = db.session.query(Currencies).filter_by(id=1).first()
 
     	assert c.id == 1
-    	assert c.name == 'USD'
+    	assert c.name == 'US Dollar'
+    	assert c.code == 'USD'
 
     def test_currencies_2(self):
 
     	currency = Currencies(
     		id=1,
-    		name='USN'
+    		name='Yuan Renminbi'
+    		code='CNY'
     	)
 
     	db.session.add(currency)
@@ -203,7 +224,8 @@ class ModelsTest(TestCase) :
     	c = db.session.query(Currencies).filter_by(id=1).first()
 
     	assert c.id == 1
-    	assert c.name == 'USN'
+    	assert c.name == 'Yuan Renminbi'
+    	assert c.code == 'CNY'
 
 
     # ---------------
@@ -303,6 +325,51 @@ class ModelsTest(TestCase) :
     	assert r.id == 1
     	assert r.name == 'Europe'
 
+
+    # ---------------
+    # SubRegions Test
+    # --------------
+
+    def test_subregions_blank(self):
+    	subregion = SubRegions(
+    		id=1
+    	)
+
+    	db.session.add(subregion)
+    	db.session.commit()
+
+    	r = db.session.query(SubRegions).filter_by(id=1).first()
+
+    	assert r.id == 1
+    	assert r.name == None
+
+    def test_subregions_1(self):
+    	subregion = SubRegions(
+    		id=2,
+    		name='Southern Europe'
+    	)
+
+    	db.session.add(subregion)
+    	db.session.commit()
+
+    	r = db.session.query(SubRegions).filter_by(id=2).first()
+
+    	assert r.id == 2
+    	assert r.name == 'Southern Europe'
+
+    def test_subregions_2(self):
+    	subregion = SubRegions(
+    		id=1,
+    		name='Northern Africa'
+    	)
+
+    	db.session.add(subregion)
+    	db.session.commit()
+
+    	r = db.session.query(SubRegions).filter_by(id=1).first()
+
+    	assert r.id == 1
+    	assert r.name == 'Southern Africa'
 
 if __name__ == "__main__" :
 	main()
