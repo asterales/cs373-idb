58d0ccdb3bc08706ce916bdafe88a4d429f94293
Alissa Hall <alissa.hall@utexas.edu>
2016-04-07 20:11:39 -0500

    tests now running without overriding database or otherwise crashing on docker

--- app/app.py
+++ app/app.py
@@ -191,11 +191,11 @@ def get_map_data(countries):
 	map_data["coords"] = coords
 	return map_data
 
+from models import create_sweography_db
+create_sweography_db()
+import populate_db
 
 if __name__ == "__main__":
-	from models import create_sweography_db
-	create_sweography_db()
-	import populate_db
 	app.run(debug=True)
 
 
